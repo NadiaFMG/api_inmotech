35bbe151f61418605d2ce1179bb7069ba0ff32f6
jonathan0145 <jonathan_bermeo014@hotmail.com>
2025-11-10 13:49:27 -0500

    manual

--- plataforma/src/utils/constants.js
+++ plataforma/src/utils/constants.js
@@ -1,8 +1,8 @@
 // // export const REACT_APP_API_BASE_URL = 'http://localhost:3000/api';
 
-// export const REACT_APP_API_BASE_URL =
-//   window.location.hostname === 'localhost'
-//     ? 'http://localhost:3000/api'
-//     : 'http://192.168.20.21:3000/api';
+export const REACT_APP_API_BASE_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:3000/api'
+    : 'http://192.168.20.21:3000/api';
 
-export const REACT_APP_API_BASE_URL = 'http://192.168.20.21:3000/api';
+// export const REACT_APP_API_BASE_URL = 'http://192.168.20.21:3000/api';
